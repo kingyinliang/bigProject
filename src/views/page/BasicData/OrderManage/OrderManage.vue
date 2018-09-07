b2ed3d8b67fbd0f9a2a2aebced155e12e523c05c
16060011 <lixiaocheng@shinho.net.cn>
2018-09-07 16:35:33 +0800

    订单管理页面调整

--- src/views/page/BasicData/OrderManage/OrderManage.vue
+++ src/views/page/BasicData/OrderManage/OrderManage.vue
@@ -37,7 +37,7 @@
                 width="122"
                 label="生产订单">
                 <template slot-scope="scope">
-                  <el-button type="text">{{scope.row.orderNo}}</el-button>
+                  <el-button type="text" style="color: #606266; padding: 9px 0;">{{scope.row.orderNo}}</el-button>
                 </template>
               </el-table-column>
               <el-table-column
