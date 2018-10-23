a0abd967c9131a28e164bd8fb044e076d3faf48d
18060064 <Wyl945521!>
2018-10-23 09:58:25 +0800

    生产环境

--- src/api/api.js
+++ src/api/api.js
@@ -6,8 +6,8 @@
 // let HOST = 'http://10.10.5.41:8080'
 // let HOST = 'http://10.1.9.133:8080'
 // let HOST = 'http://10.10.1.58:8080'
-// let HOST = 'https://mdsdevsys.shinho.net.cn'// 生产环境
-let HOST = 'https://mdsuatsys.shinho.net.cn'// 测试环境dev
+let HOST = 'https://mdsdevsys.shinho.net.cn'// 生产环境
+// let HOST = 'https://mdsuatsys.shinho.net.cn'// 测试环境dev
 // let HOST = 'https://mdssys.shinho.net.cn'// 正式环境master
 /*
 *MAINapi
