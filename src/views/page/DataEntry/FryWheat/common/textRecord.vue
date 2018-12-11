1fdd3880927f0ca1d50355dc66dce9ee44ac4083
18060064 <Wyl945521!>
2018-12-11 11:30:52 +0800

    啦啦啦  炒麦车间

---
+++ src/views/page/DataEntry/FryWheat/common/textRecord.vue
@@ -0,0 +1,27 @@
+<template>
+  <div><el-input type="textarea" v-model="Text" class="textarea" style="width: 100%;height: 200px"></el-input></div>
+</template>
+
+<script>
+export default {
+  name: 'textRecord',
+  data () {
+    return {}
+  },
+  props: {
+    Text: {}
+  },
+  mounted () {
+  },
+  methods: {},
+  computed: {},
+  components: {}
+}
+</script>
+
+<style scoped>
+.textarea textarea{
+  width: 100%;
+  height: 100%!important;
+}
+</style>
