64034cfa9ac3a1af417b16f3966c40b009c43267
16080029 <maxuehan@shinho.net.cn>
2019-01-18 09:25:08 +0800

    炒麦api

--- src/api/api.js
+++ src/api/api.js
@@ -223,6 +223,10 @@ export const SYSTEMSETUP_API = {
    */
   PARAMETERLIST_API: HOST + '/sys/dict/findListByType',
   /**
+   * @property {string} PARAMETERSLIST_API 参数类型列表
+   */
+  PARAMETERSLIST_API: HOST + '/sys/dict/findListByTypes',
+  /**
    * @property {string} PARAMETERTYPE_API 参数类型
    */
   PARAMETERTYPE_API: HOST + '/sys/dict/list',
@@ -697,4 +701,105 @@ export const REP_API = {
    */
   REPOUTPUTMANHOUR_API: HOST + '/report/formh/totalHoursList',
   REPOUTPUTMANHOUREXPORT_API: HOST + '/report/formh/exportTotalHours'
+}
+/*
+*炒麦api
+ */
+export const WHT_API = {
+  /**
+   * @property {string} READYTIME_API 准备时间修改
+   */
+  READYTIMEUPDATE_API: HOST + '/wht/ready/whtReadyUpdate',
+  /**
+   * @property {string} READYTIMELIST_API 获取准备时间
+   */
+  READYTIMELIST_API: HOST + '/wht/ready/whtReadyList',
+  /**
+   * @property {string} MACHINEIMELIST_API 获取机器工时
+   */
+  MACHINETIMELIST_API: HOST + '/wht/ready/whtReadyList',
+  /**
+   * @property {string} MACHINEIMEUPDATE_API 机器工时修改
+   */
+  MACHINETIMEUPDATE_API: HOST + '/wht/ready/whtMachineUpdate',
+  /**
+   * @property {string} MACHINETESTUPDATE_API 机器检测列表
+   */
+  MACHINETESTlist_API: HOST + '/wht/ready/whtCheckRecordList',
+  /**
+   * @property {string} MACHINETESTUPDATE_API 机器检测列表
+   */
+  MACHINETESTUPDATE_API: HOST + '/wht/ready/whtCheckRecordUpdate',
+  /**
+   * 首页人员列表
+   */
+  CINDEXLISTUSER: HOST + '/wht/user/listUser',
+  /**
+   * 首页人员新增修改
+   */
+  CINDEXUPDATEUSER: HOST + '/wht/user/updateUser',
+  /**
+   * 首页人员删除
+   */
+  CINDEXDELUSER: HOST + '/wht/user/delUser',
+  /**
+   * 首页订单接口
+   */
+  CINDEXORDERLIST_API: HOST + '/wht/order/list',
+  /**
+   * @property {string} INSTORAGELIST_API 生产入库列表
+   */
+  INSTORAGELIST_API: HOST + '/sys/whtInStorage/list',
+  /**
+   * @property {string} INSTORAGESAVE_API 生产入库保存
+   */
+  INSTORAGESAVE_API: HOST + '/sys/whtInStorage/update',
+  /**
+   * @property {string} INSTORAGESUBMIT_API 生产入库提交
+   */
+  INSTORAGESUBMIT_API: HOST + '/sys/whtInStorage/submit',
+  /**
+   * @property {string} MATERIELSAVEORDER_API pw小麦申请订单
+   */
+  MATERIELSAVEORDER_API: HOST + '/sys/whtPwMaterial/getSapOrder',
+  /**
+   * @property {string} MATERIELLIST_API pw小麦list
+   */
+  MATERIELLIST_API: HOST + '/sys/whtPwMaterial/list',
+  /**
+   * @property {string} MATERIELSAVE_API pw小麦保存
+   */
+  MATERIELSAVE_API: HOST + '/sys/whtPwMaterial/update',
+  /**
+   * @property {string} MATERIELSUBMIT_API pw小麦提交
+   */
+  MATERIELSUBMIT_API: HOST + '/sys/whtPwMaterial/submit',
+  /**
+   * @property {string} MATERIELTIMELIST_API pw小麦工时list
+   */
+  MATERIELTIMELIST_API: HOST + '/wht/pw/timeList',
+  /**
+   * @property {string} MATERIELTIMEUPDATE_API pw小麦工时update
+   */
+  MATERIELTIMEUPDATE_API: HOST + '/wht/pw/timeUpdate',
+  /**
+   * @property {string} MATERIELTIMESUBMIT_API pw小麦工时提交
+   */
+  PWMATERIELTIMESUBMIT_API: HOST + '/wht/pw/submitPW',
+  /**
+   * @property {string} MATERIELTIMESUBMIT_API 炒麦报工提交
+   */
+  MATERIELTIMESUBMIT_API: HOST + '/wht/ready/submitW',
+  /**
+   * @property {string} APPLYMATERIELLIST_API 物料申请list
+   */
+  APPLYMATERIELLIST_API: HOST + '/wht/material/list',
+  /**
+   * @property {string} APPLYMATERIELSAVE_API 物料申请保存
+   */
+  APPLYMATERIELSAVE_API: HOST + '/wht/material/updateM',
+  /**
+   * @property {string} APPLYMATERIELSUBMIT_API 物料申请提交
+   */
+  APPLYMATERIELSUBMIT_API: HOST + '/wht/material/submitM'
 }
