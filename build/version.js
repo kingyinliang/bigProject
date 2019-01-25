abdd5eee00618b789d2fa351ce5c6fdf1613569f
18060064 <Wyl945521!>
2019-01-25 15:09:45 +0800

    version

--- build/version.js
+++ build/version.js
@@ -1,7 +1,16 @@
 const fs = require('fs')
 
 let version = (new Date()).valueOf()
-fs.writeFile('src/version.js', `exports.version = ${version}
+fs.writeFile('src/version.js', `export const.version = ${version}
+console.log(version)
+`, (error) => {
+  if (error) {
+    console.log(error)
+  } else {
+    console.log('sucess! version: ',version)
+  }
+})
+fs.writeFile('src/nodeversion.js', `exports.version = ${version}
 `, (error) => {
   if (error) {
     console.log(error)
--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,6 @@
 'use strict'
 // let gitSha = require('child_process').execSync('git rev-parse HEAD').toString().trim()
-let gitSha = require('../src/version')
+let gitSha = require('../src/nodeversion').version
 
 process.env.RELEASE_VERSION = gitSha
 module.exports = {
