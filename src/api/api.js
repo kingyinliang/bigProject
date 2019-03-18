5c3cf2b1dd59a6fd0c26441f203c4b918d961b5d
16080029 <maxuehan@shinho.net.cn>
2019-03-18 14:38:37 +0800

    煮豆 api

--- src/api/api.js
+++ src/api/api.js
@@ -7,7 +7,7 @@
 // let HOST = 'http://10.10.5.40:8080/xhqy-fc'
 // let HOST = 'http://10.10.1.167:8080'
 // let HOST = 'http://10.10.5.40:8080/xhqy-fc' // zl
-// let HOST = 'http://10.10.1.83:8080/xhqy-fc' // yc
+// let HOST = 'http://10.10.1.151:8080/xhqy-fc' // yc
 // HOST
 let HOST = 'https://apimarket-dev.shinho.net.cn/xhqy-fc' // 开发环境
 // let HOST = 'http://alb001-ecs-1995142321.cn-north-1.elb.amazonaws.com.cn/xhqy-fc' // 开发环境alb
