daaa92ae24f7d0d6d086568ac01918426400859a
18060064 <Wyl945521!>
2019-04-18 17:37:53 +0800

    一个页面拆分三个

---
+++ src/views/page/DataEntry/KojiMaking/Out.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <index :name="name"></index>
+  </div>
+</template>
+
+<script>
+import index from './index'
+export default {
+  name: 'Out',
+  data () {
+    return {
+      name: 'Out'
+    }
+  },
+  mounted () {
+  },
+  methods: {},
+  computed: {},
+  components: {
+    index
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
