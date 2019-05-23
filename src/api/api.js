25f5e8b6e6d1fa8d32225c5ee48438217b93e892
18060064 <Wyl945521!>
2019-05-23 10:34:32 +0800

    测试 可回滚

--- src/api/api.js
+++ src/api/api.js
@@ -1,20 +1,4 @@
-
-let HOST
-switch (process.env.srcconfig) {
-  case 'dev':
-    HOST = 'https://apimarket-dev.shinho.net.cn/xhqy-fc'
-    break
-  case 'test':
-    HOST = 'https://apimarket-test.shinho.net.cn/xhqy-fc'
-    break
-  case 'pro':
-    HOST = 'https://apimarket.shinho.net.cn/xhqy-fc'
-    break
-  default:
-    HOST = 'https://apimarket-dev.shinho.net.cn/xhqy-fc'
-    // HOST = 'http://10.10.1.62:8080/xhqy-fc'
-    break
-}
+let HOST = require('./host.js').HOST
 /*
 *MAINapi
  */
