fab6a3d39165618643f36da6ab451f2d31eb51bc
18060064 <Wyl945521!>
2019-05-30 17:21:33 +0800

    cfs攻击

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,14 @@ export default {
   name: 'App',
   mounted () {
     // if (this.$route.meta.title) document.title = this.$route.meta.title
+    var HOST = require('./api/location-' + process.env.NODE_ENV).HOST
+    var top = window.location.href
+    top = top.split('//')[1].split('/')[0]
+    if (top === HOST) {
+      document.documentElement.style.visibility = 'visible'
+    } else {
+      window.location.href = `https://${HOST}`
+    }
   }
 }
 </script>
@@ -16,4 +24,7 @@ export default {
 <style>
 @import "assets/css/base.css";
 @import "assets/icon/iconfont.css";
+  html{
+    visibility: hidden;
+  }
 </style>
