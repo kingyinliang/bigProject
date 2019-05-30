219fd7a0e3a943b48ccd82645ba3a6bdd201df28
18060064 <Wyl945521!>
2019-05-30 10:48:27 +0800

    发uat

--- src/api/host-development.js
+++ src/api/host-development.js
@@ -1,8 +1,9 @@
 
 let HOST
 // HOST = 'https://apimarket-dev.shinho.net.cn/xhqy-fc' // 开发环境
-HOST = 'http://10.10.1.17:8080/xhqy-fc' // zl
+HOST = 'http://10.10.1.29:8080/xhqy-fc' // zl
 // HOST = 'http://10.10.1.188:8080/xhqy-fc' // yc
+// HOST = 'http://localhost:3000/xhqy-fc' // 本地
 
 module.exports = {
   HOST
