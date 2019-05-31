ee2c98aaeccee80ca356be094f75494d8de009d3
18060064 <Wyl945521!>
2019-05-31 10:29:59 +0800

    粮仓

---
+++ src/views/page/DataEntry/Granary/BeanPulp/dataEntryIndex.vue
@@ -0,0 +1,21 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: 'dataEntryIndex',
+  data () {
+    return {}
+  },
+  mounted () {
+  },
+  methods: {},
+  computed: {},
+  components: {}
+}
+</script>
+
+<style scoped>
+
+</style>
