e782403999dc8dc5f6ef892ae6e6b5851f708546
18060064 <Wyl945521!>
2019-06-18 14:55:01 +0800

    发酵罐

---
+++ src/views/page/DataEntry/Fermentation/Fermenter/details.vue
@@ -0,0 +1,24 @@
+<template>
+  <div style="padding: 10px">
+    <el-card class="searchCard  newCard">
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Ferdetails',
+  data () {
+    return {}
+  },
+  mounted () {
+  },
+  methods: {},
+  computed: {},
+  components: {}
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/page/DataEntry/Fermentation/Fermenter/index.vue
@@ -0,0 +1,27 @@
+<template>
+<div>
+  <el-button type="primary" size="small" @click="godetails">详情</el-button>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'index',
+  data () {
+    return {}
+  },
+  mounted () {
+  },
+  methods: {
+    godetails () {
+      this.$router.push({ name: `DataEntry-Fermentation-Fermenter-details` })
+    }
+  },
+  computed: {},
+  components: {}
+}
+</script>
+
+<style scoped>
+
+</style>
