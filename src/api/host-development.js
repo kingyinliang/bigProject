08611e7c36b6ad30ce33289ed5ad9b0486cf15d1
18060064 <Wyl945521!>
2019-07-05 18:41:30 +0800

    fix(host): devhost
    devhost

--- src/api/host-development.js
+++ src/api/host-development.js
@@ -1,6 +1,6 @@
 
 let HOST
-HOST = 'https://apimarket.shinho.net.cn/xhqy-fc' // 开发环境
+HOST = 'https://apimarket-dev.shinho.net.cn/xhqy-fc' // 开发环境
 // HOST = 'http://10.10.1.76:8080/xhqy-fc' // zl
 // HOST = 'http://10.10.1.188:8080/xhqy-fc' // yc
 // HOST = 'http://10.10.1.27:8080/xhqy-fc' // xj
