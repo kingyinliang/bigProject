4da0a18d7b149364191a12c34e80cff832d11783
18060064 <Wyl945521!>
2019-07-31 18:14:16 +0800

    feat(host): host切回aws
    host切回aws

--- src/api/host-production.js
+++ src/api/host-production.js
@@ -7,20 +7,20 @@ if (type === undefined) {
 } else {
   switch (type) {
     case 'dev':
-      // HOST = 'https://apimarket-dev.shinho.net.cn/xhqy-fc'
-      HOST = 'http://ALB001-ECS-1995142321.cn-north-1.elb.amazonaws.com.cn/xhqy-fc'
+      HOST = 'https://apimarket-dev.shinho.net.cn/xhqy-fc'
+      // HOST = 'http://ALB001-ECS-1995142321.cn-north-1.elb.amazonaws.com.cn/xhqy-fc'
       break
     case 'test':
-      // HOST = 'https://apimarket-test.shinho.net.cn/xhqy-fc'
-      HOST = 'http://ALB002-ECS-1150488715.cn-north-1.elb.amazonaws.com.cn/xhqy-fc'
+      HOST = 'https://apimarket-test.shinho.net.cn/xhqy-fc'
+      // HOST = 'http://ALB002-ECS-1150488715.cn-north-1.elb.amazonaws.com.cn/xhqy-fc'
       break
     case 'pro':
-      // HOST = 'https://apimarket.shinho.net.cn/xhqy-fc'
-      HOST = 'http://internal-ALB003-Shinho-Micro-PRD-929565893.cn-north-1.elb.amazonaws.com.cn/xhqy-fc'
+      HOST = 'https://apimarket.shinho.net.cn/xhqy-fc'
+      // HOST = 'http://internal-ALB003-Shinho-Micro-PRD-929565893.cn-north-1.elb.amazonaws.com.cn/xhqy-fc'
       break
     default:
-      // HOST = 'https://apimarket-dev.shinho.net.cn/xhqy-fc'
-      HOST = 'http://ALB001-ECS-1995142321.cn-north-1.elb.amazonaws.com.cn/xhqy-fc'
+      HOST = 'https://apimarket-dev.shinho.net.cn/xhqy-fc'
+      // HOST = 'http://ALB001-ECS-1995142321.cn-north-1.elb.amazonaws.com.cn/xhqy-fc'
       break
   }
 }
