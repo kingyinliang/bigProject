9853628fb8626049738756e30ec5e3800164f136
18060064 <Wyl945521!>
2019-07-31 16:35:59 +0800

    feat(host): 切换albhost
    host

--- src/api/host-production.js
+++ src/api/host-production.js
@@ -8,19 +8,19 @@ if (type === undefined) {
   switch (type) {
     case 'dev':
       // HOST = 'https://apimarket-dev.shinho.net.cn/xhqy-fc'
-      HOST = 'http://ALB001-ECS-1995142321.cn-north-1.elb.amazonaws.com.cn'
+      HOST = 'http://ALB001-ECS-1995142321.cn-north-1.elb.amazonaws.com.cn/xhqy-fc'
       break
     case 'test':
       // HOST = 'https://apimarket-test.shinho.net.cn/xhqy-fc'
-      HOST = 'http://ALB002-ECS-1150488715.cn-north-1.elb.amazonaws.com.cn'
+      HOST = 'http://ALB002-ECS-1150488715.cn-north-1.elb.amazonaws.com.cn/xhqy-fc'
       break
     case 'pro':
       // HOST = 'https://apimarket.shinho.net.cn/xhqy-fc'
-      HOST = 'http://internal-ALB003-Shinho-Micro-PRD-929565893.cn-north-1.elb.amazonaws.com.cn'
+      HOST = 'http://internal-ALB003-Shinho-Micro-PRD-929565893.cn-north-1.elb.amazonaws.com.cn/xhqy-fc'
       break
     default:
       // HOST = 'https://apimarket-dev.shinho.net.cn/xhqy-fc'
-      HOST = 'http://ALB001-ECS-1995142321.cn-north-1.elb.amazonaws.com.cn'
+      HOST = 'http://ALB001-ECS-1995142321.cn-north-1.elb.amazonaws.com.cn/xhqy-fc'
       break
   }
 }
